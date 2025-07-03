b82f14d306702f2c70e0d50ef4ac4ff2e29e843b
Zouhair <zouhairelammari06@example.com>
2025-07-03 18:09:21 +0100

    add files

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -19,7 +19,7 @@ export const AppContextProvider = (props) => {
 
     const [products, setProducts] = useState([])
     const [userData, setUserData] = useState(false)
-    const [isSeller, setIsSeller] = useState(true)
+    const [isSeller, setIsSeller] = useState(false)
     const [cartItems, setCartItems] = useState({})
 
     const fetchProductData = async () => {
